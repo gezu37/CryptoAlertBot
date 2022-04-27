5e5c73d41ab5fb4161ab8f7d89867aa8089eef19
gezu37 <mrkirillcatplays@mail.ru>
2022-04-27 23:21:32 +0300

    .

---
+++ BTCupdates.py
@@ -0,0 +1,11 @@
+from aiogram import Dispatcher, types
+
+
+async def cmd_subscribe(message: types.Message):
+    keyboard = types.InlineKeyboardMarkup()
+    keyboard.add(types.InlineKeyboardButton(text="Подписаться", callback_data="subscribe"))
+    await message.answer("Подписаться на уведомления?", reply_markup=keyboard)
+
+
+def register_handlers_subscribe(dp: Dispatcher):
+    dp.register_message_handler(cmd_subscribe, commands="BTC_price_updates")
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import asyncio
+import csv
 from aiogram.types.bot_command import BotCommand
+from aiogram.utils.exceptions import BotBlocked
 from trend import register_handlers_trend
 from MostSearch import register_handlers_searched
+from BTCupdates import register_handlers_subscribe
 import logging
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot = Bot(token="5088468387:AAHUbnEmXjZhDjSLefJ79nvpvVsAGS9_1qA", parse_mode=types.ParseMode.HTML)
@@ -16,9 +19,25 @@ logger = logging.getLogger(__name__)
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(command="/trend", description="Показать максимальные рост и падения за последние сутки"),
-        BotCommand(command="/searched", description="Показать какие монеты чаще всего искали в последнее время")
+        BotCommand(command="/searched", description="Показать какие монеты чаще всего искали в последнее время"),
+        BotCommand(command="/BTC_price_updates", description="Ежедневные уведомления о цене BTC")
     ]
     await bot.set_my_commands(commands)
+
+
+@dp.errors_handler(exception=BotBlocked)
+async def error_bot_blocked(update: types.Update, exception: BotBlocked):
+    print(f"Меня заблокировал пользователь!\nСообщение: {update}\nОшибка: {exception}")
+    return True
+
+
+@dp.callback_query_handler(text="subscribe")
+async def subscribe(call: types.CallbackQuery):
+    user_id = call.from_user.id
+    with open('users_id.csv', mode='w') as users_id:
+        user_writer = csv.writer(users_id, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        user_writer.writerow(user_id)
+    await call.message.answer("Вы подписались на уведомления по изменению цены Bitcoin")
 
 
 async def main():
@@ -29,6 +48,7 @@ async def main():
     logger.error("Starting bot")
     register_handlers_trend(dp)
     register_handlers_searched(dp)
+    register_handlers_subscribe(dp)
 
     await set_commands(bot)
     await dp.skip_updates()

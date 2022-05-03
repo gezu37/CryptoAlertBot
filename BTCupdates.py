45c1c8fae1397bec2e8bb5e7f1acb18cbdf23389
gezu37 <mrkirillcatplays@mail.ru>
2022-05-03 21:09:51 +0300

    .

--- BTCupdates.py
+++ BTCupdates.py
@@ -1,10 +1,25 @@
 from aiogram import Dispatcher, types
+from bs4 import BeautifulSoup
+import requests
+from fake_useragent import UserAgent
+
+agent = UserAgent()
 
 
 async def cmd_subscribe(message: types.Message):
     keyboard = types.InlineKeyboardMarkup()
     keyboard.add(types.InlineKeyboardButton(text="Подписаться", callback_data="subscribe"))
     await message.answer("Подписаться на уведомления?", reply_markup=keyboard)
+
+
+def btcprice_collect():
+    response = requests.get(
+        url='https://coinmarketcap.com/currencies/bitcoin/',
+        headers={'user-agent': f'{agent.random}'})
+    soup = BeautifulSoup(response.text, "html.parser")
+    price = soup.find('div', class_='priceValue', )
+    price_rez = price.find('span').text
+    return price_rez
 
 
 def register_handlers_subscribe(dp: Dispatcher):
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import asyncio
 import csv
+from BTCupdates import btcprice_collect
 from aiogram.types.bot_command import BotCommand
 from aiogram.utils.exceptions import BotBlocked
 from trend import register_handlers_trend
 from MostSearch import register_handlers_searched
 from BTCupdates import register_handlers_subscribe
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot = Bot(token="5088468387:AAHUbnEmXjZhDjSLefJ79nvpvVsAGS9_1qA", parse_mode=types.ParseMode.HTML)
@@ -33,12 +34,36 @@ async def error_bot_blocked(update: types.Update, exception: BotBlocked):
 
 @dp.callback_query_handler(text="subscribe")
 async def subscribe(call: types.CallbackQuery):
-    user_id = str(call.from_user.id)
-    with open('users_id.csv', mode='w') as users_id:
-        user_writer = csv.writer(users_id, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        user_writer.writerow(user_id)
-    await call.message.answer("Вы подписались на уведомления по изменению цены Bitcoin")
-    await call.answer()
+    user_id_str = str(call.from_user.id)
+    user_id = [user_id_str]
+    detector = 0
+    with open('users_id.csv', 'r') as users_id:
+        user_reader = csv.reader(users_id)
+        for row in user_reader:
+            if row == user_id:
+                detector = 1
+
+    if detector == 0:
+        with open(r'users_id.csv', 'a') as users_id:
+            user_writer = csv.writer(users_id)
+            user_writer.writerow(user_id)
+        await call.message.answer("Вы подписались на уведомления по изменению цены Bitcoin")
+        await call.answer()
+    else:
+        await call.message.answer("Вы уже подписаны")
+        await call.answer()
+
+
+async def notifications(sleep_for):
+    while True:
+        await asyncio.sleep(sleep_for)
+
+        with open('users_id.csv', 'r') as users_id:
+            user_reader = csv.reader(users_id)
+            for row in user_reader:
+                chat = int(row[0])
+                price = btcprice_collect()
+                await bot.send_message(chat, f"Цена Bitcoin (BTC) на текущий момент {price}")
 
 
 async def main():
@@ -56,6 +81,8 @@ async def main():
     await dp.skip_updates()
     await dp.start_polling()
 
+
 if __name__ == '__main__':
     asyncio.run(main())
-    
+
+
